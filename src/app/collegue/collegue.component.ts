8e63fca06727fed4266b1b5fa9397e8c8233aece
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2020-02-09 01:11:36 +0100

    TP2 Création composant collègue

---
+++ src/app/collegue/collegue.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+
+@Component({
+  selector: 'app-collegue',
+  templateUrl: './collegue.component.html',
+  styleUrls: ['./collegue.component.css']
+})
+export class CollegueComponent implements OnInit {
+
+  constructor() {}
+
+  @Input() col: Collegue;
+
+  creerCollegue(){
+    console.log('Créer un nouveau collègue');
+  }
+
+  modifierCollegue(){
+    console.log('Modification du collègue');
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,4 @@
+import {Collegue} from '../models/Collegue';
+
+export const collegueMock = new Collegue('XS189657325DF', 'MACRON', 'Emmanuel', 'e.macron@mail.com',
+ new Date('1977-12-21'), 'https://www.unamur.be/en/sci/chemistry/rco/membres-images/inconnu/image');
